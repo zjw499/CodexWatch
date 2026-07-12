f572bb888b80722ef241ab30ee7c0747816b563c
zjw499 <zjw499@gmail.com>
2026-07-11 21:06:01 -0400

    Publish watchos/CodexWatch/AppConfiguration.swift

--- watchos/CodexWatch/AppConfiguration.swift
+++ watchos/CodexWatch/AppConfiguration.swift
@@ -8,15 +8,4 @@ enum CodexWatchConfiguration {
 
     static let relayToken: String? = Bundle.main.object(forInfoDictionaryKey: "CODEX_WATCH_RELAY_TOKEN") as? String
 
-    static let audioUploadURL: URL? = {
-        guard let value = Bundle.main.object(forInfoDictionaryKey: "CODEX_WATCH_AUDIO_UPLOAD_URL") as? String,
-              !value.isEmpty,
-              !value.contains("REPLACE_WITH") else {
-            return nil
-        }
-        return URL(string: value)
-    }()
-
-    static let audioUploadUsername = Bundle.main.object(forInfoDictionaryKey: "CODEX_WATCH_AUDIO_UPLOAD_USERNAME") as? String
-    static let audioUploadPassword = Bundle.main.object(forInfoDictionaryKey: "CODEX_WATCH_AUDIO_UPLOAD_PASSWORD") as? String
 }
